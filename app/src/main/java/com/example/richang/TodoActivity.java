6e7c65eb35d9a83a851cd2e8b2295ce9d86da3e8
polan233 <1292437680@qq.com>
2022-11-14 11:33:54 +0800

    todo前端搭好了

--- app/src/main/java/com/example/richang/TodoActivity.java
+++ app/src/main/java/com/example/richang/TodoActivity.java
@@ -3,12 +3,24 @@ package com.example.richang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TodoActivity extends AppCompatActivity {
-
+    private TextView nav_eat;
+    private TextView nav_habit;
+    private TextView nav_dairy;
+    private Button btn_delete;
+    private Button btn_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+
+        nav_eat=(TextView) findViewById(R.id.tv_eat);
+        nav_habit=(TextView) findViewById(R.id.tv_habit);
+        nav_dairy=(TextView) findViewById(R.id.tv_dairy);
+        btn_delete=(Button) findViewById(R.id.btn_delete);
+        btn_add=(Button) findViewById(R.id.btn_add);
     }
 }
