e8c4a78669d3f76ff10c8a06770b407aaea3b790
polan233 <1292437680@qq.com>
2022-11-17 22:26:33 +0800

    todo页面优化不少了,今天先这样

--- app/src/main/java/com/example/richang/TodoAdapter.java
+++ app/src/main/java/com/example/richang/TodoAdapter.java
@@ -3,6 +3,7 @@ package com.example.richang;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.List;
+import java.util.TimerTask;
 
 public class TodoAdapter extends ArrayAdapter<Todo> {
     private List<Todo> data=null;
@@ -48,10 +50,28 @@ public class TodoAdapter extends ArrayAdapter<Todo> {
 //                    animation.start();
 //                    while(animation.isRunning()){};
 //                }
+                btn.setText("☑");
+                ObjectAnimator animation1=ObjectAnimator.ofFloat(ll,"scaleX",0f);
+                ObjectAnimator animation2=ObjectAnimator.ofFloat(ll,"translationX",-1000);
+                animation1.setDuration(600);
+                animation2.setDuration(600);
+                animation1.start();
+                animation2.start();
+                //TODO 这里就可以加一些动画效果啦
 
-                deleteTodo(todo);
-                data.remove(position);
-                notifyDataSetChanged();
+                Handler handler = new Handler(); // 如果这个handler是在UI线程中创建的
+                handler.postDelayed(new Runnable() {  // 开启的runnable也会在这个handler所依附线程中运行，即主线程
+                    @Override
+                    public void run() {
+                        // 可更新UI或做其他事情
+                        // 注意这里还在当前线程，没有开启新的线程
+                        // new Runnable(){}，只是把Runnable对象以Message形式post到UI线程里的Looper中执行，并没有新开线程。
+                        deleteTodo(todo);
+                        data.remove(position);
+                        notifyDataSetChanged();
+                    }
+                }, 600); // 延时0.6s执行run内代码
+
             }
         });
         return view;
